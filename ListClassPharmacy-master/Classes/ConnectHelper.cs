efdd5c9ed780b9992e4f2f7cff8ae34c6b9d07ff
SuhanovaE <98471539+SuhanovaE@users.noreply.github.com>
2022-05-09 17:44:36 +0300

    Add files via upload

---
+++ ListClassPharmacy-master/Classes/ConnectHelper.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ListClass.Classes
+{/// <summary>
+/// вспомогательный класс
+/// </summary>
+    class ConnectHelper
+    {
+        public static List<Student> students = new List<Student>();
+        public static void ReadListFromFile(string filename)
+        {
+            StreamReader streamReader = new StreamReader(filename, Encoding.UTF8);
+            while (!streamReader.EndOfStream)
+            {
+                string line = streamReader.ReadLine();
+                string[] items = line.Split(',');
+                Student student = new Student()
+                {
+                    NameStudent = items[0].Trim(),
+                    NamegGroup = items[1].Trim(),
+                    CountGymnastic = int.Parse(items[2].Trim()),
+                    CountChemistry = int.Parse(items[3].Trim()),
+                    CountPhysics = int.Parse(items[4].Trim()),
+                    CountAlgebra = int.Parse(items[5].Trim()),
+                    CountLiterature = int.Parse(items[6].Trim())
+                };
+                students.Add(student);
+            }
+        }
+        public static void SaveListToFile(string filename)
+        {
+            StreamWriter streamWriter = new StreamWriter(filename, false, Encoding.UTF8);
+            foreach (Student st in students)
+            {
+                streamWriter.WriteLine($"{st.NameStudent};{st.NamegGroup};{st.CountGymnastic};{st.CountChemistry};{st.CountPhysics};{st.CountAlgebra};{st.CountLiterature}");
+            }
+            streamWriter.Close();
+        }
+    }
+}
---
+++ ListClassPharmacy-master/WindowAddStudent.xaml.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using ListClass.Classes;
+
+namespace ListClass
+{
+    /// <summary>
+    /// Логика взаимодействия для WindowAddStudent.xaml
+    /// </summary>
+    public partial class WindowAddPreparate : Window
+    {
+        int mode;
+        public WindowAddPreparate()
+        {
+            InitializeComponent();
+            mode = 0;
+        }
+        public WindowAddPreparate(Student st)
+        {
+            InitializeComponent();
+            TxbName.Text = st.NameStudent;
+            TxbGroup.Text = st.NamegGroup;
+            TxbGymnastic.Text = st.CountGymnastic.ToString();
+            TxbChemistry.Text = st.CountChemistry.ToString();
+            TxbPhysics.Text = st.CountPhysics.ToString();
+            TxbAlgebra.Text = st.CountAlgebra.ToString();
+            TxbLiterature.Text = st.CountLiterature.ToString();
+            mode = 1;
+            BtnAddStudent.Content = "Сохранить";
+        }
+
+        private void BtnAddStudent_Click(object sender, RoutedEventArgs e)
+        {
+            if (mode == 0)
+            {
+                try
+                {
+                    Student student = new Student()
+                    {
+                        NameStudent = TxbName.Text,
+                        NamegGroup = TxbGroup.Text,
+                        CountGymnastic = int.Parse(TxbGymnastic.Text),
+                        CountChemistry = int.Parse(TxbChemistry.Text),
+                        CountPhysics = int.Parse(TxbPhysics.Text),
+                        CountAlgebra = int.Parse(TxbAlgebra.Text),
+                        CountLiterature = int.Parse(TxbLiterature.Text)
+                    };
+                    ConnectHelper.students.Add(student);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Проверьте входные данные: {ex}", "Ошибка!", MessageBoxButton.OK, MessageBoxImage.Error);
+                    return;
+                }
+
+            }
+            else
+            {
+                try
+                {
+                    for (int i = 0; i < ConnectHelper.students.Count; i++)
+                    {
+                        if (ConnectHelper.students[i].NameStudent == TxbName.Text)
+                        {
+                            ConnectHelper.students[i].NamegGroup = TxbGroup.Text;
+                            ConnectHelper.students[i].CountGymnastic = int.Parse(TxbGymnastic.Text);
+                            ConnectHelper.students[i].CountChemistry = int.Parse(TxbChemistry.Text);
+                            ConnectHelper.students[i].CountPhysics = int.Parse(TxbPhysics.Text);
+                            ConnectHelper.students[i].CountAlgebra = int.Parse(TxbAlgebra.Text);
+                            ConnectHelper.students[i].CountLiterature = int.Parse(TxbLiterature.Text);
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Проверьте входные данные: {ex}", "Ошибка!", MessageBoxButton.OK, MessageBoxImage.Error);
+                    return;
+                }
+
+            }
+            ConnectHelper.SaveListToFile(@"ListStudents.txt");
+            this.Close();
+        }
+    }
+}
